443424a091119bd64025b45e8398983ae4678cf2
Gilbert Marín <daviddlioncourt@gmail.com>
2021-03-09 08:51:27 +0000

    Carta Aleatoria Done

--- index.js
+++ index.js
@@ -1,30 +1,20 @@
 window.onload = function() {
   
-    let imagen = document.getElementsByName("tipoCarta");
-    let numero = document.getElementById("Numero");
+    let imagen = document.getElementById("type");
+    let numero = document.getElementById("numero");
 
-    let cardnumber = ["1","2","3","4","5","6","7","8","9","10","J","Q","K","A"];
-    let tipoCarta = ["https://dliziasexpress.webnode.cr/_files/200000101-b76c1b76c3/200/ScreenHunter_129%20Mar.%2008%2020.43.jpg",
-     "https://dliziasexpress.webnode.cr/_files/200000100-143191431b/200/ScreenHunter_128%20Mar.%2008%2020.43.jpg",
-      "https://dliziasexpress.webnode.cr/_files/200000099-3c4983c49b/200/ScreenHunter_127%20Mar.%2008%2020.43.jpg",
-       "https://dliziasexpress.webnode.cr/_files/200000098-40abf40ac2/200/ScreenHunter_126%20Mar.%2008%2020.43.jpg"];
+    let cardnumber = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"];
+    let tipoCarta = ["spades", "hearts", "clubs", "diamonds"];
 
     
     var tipoCartaSelec = Math.floor(Math.random(tipoCarta.length)*4);
-    let tipo = imagen.querySelector("img scr");
-    tipo.innerHTML = tipoCarta[tipoCartaSelec];
+    let tipo = imagen.classList.add(tipoCarta[tipoCartaSelec]);
+    
 
 
     var cardselec = Math.floor(Math.random(cardnumber.length)*14);
-    let num = numero.querySelector("h1");
+    let num = numero.querySelector("p");
     num.innerHTML = cardnumber[cardselec];
-
-
-
-
-
-
-    
 
 
 };
